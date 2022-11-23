e8c003cb4357184970bf7306c5a1efdc85df4187
Funt <funt_88@mail.ru>
2022-11-24 01:53:41 +0300

    task_2 completed

---
+++ Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int counter = Convert.ToString(number).Length;
+int summ = 0;
+if (number < 0)
+{
+    number *= -1;
+}
+while (counter > 0)
+{
+    summ = summ + number % 10;
+    number /= 10;
+    counter--;
+}
+Console.WriteLine(summ);
