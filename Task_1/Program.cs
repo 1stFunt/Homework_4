86f8cac96aeafa3e275af432131d695d9fef7f28
Funt <funt_88@mail.ru>
2022-11-23 23:52:42 +0300

    task_1 completed

---
+++ Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Без использования функции Math.Pow.
+
+int number1 = ReadInt("Введите первое число A: ");
+int number2 = ReadInt("Введите второе число B: ");
+int count = 1;
+int degree = number1;
+while (count < number2)
+{
+    degree = degree * number1;
+    count++;
+}
+Console.WriteLine(degree);
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
