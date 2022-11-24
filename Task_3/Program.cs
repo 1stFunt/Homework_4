48e2b9c43fcc76764aaffc945b334b9d3ac4c93b
Funt <funt_88@mail.ru>
2022-11-24 03:47:01 +0300

    task_3 completed

---
+++ Task_3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран. Вывод сделать отдельным методом.
+
+Console.WriteLine("Введите натуральное число (N): ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[number];
+
+void FillArray(int[] coll)
+{
+    int length = coll.Length;
+    int index = 0;
+    while (index < length)
+    {
+        coll[index] = new Random().Next(1, 100);
+        index++;
+    }
+}
+void PrintArray(int[] coll)
+{
+    int length = coll.Length;
+    int index = 0;
+    while (index < length - 1)
+    {
+        Console.Write($"{coll[index]},");
+        index++;
+    }
+    if (index == length - 1)
+    {
+        Console.Write($"{coll[index]}]");
+    }
+}
+Console.Write($"{number}->[");
+FillArray(array);
+PrintArray(array);
